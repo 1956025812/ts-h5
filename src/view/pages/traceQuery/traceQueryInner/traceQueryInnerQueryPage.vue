ed49acf4cb2e606df73d8a4ba421bdaca7ada14e
请叫我猿叔叔 <1956025812@qq.com>
2019-08-29 10:44:36 +0800

    轨迹查询内部 页面分割 OK

--- src/view/pages/traceQuery/traceQueryInner/traceQueryInnerQueryPage.vue
+++ src/view/pages/traceQuery/traceQueryInner/traceQueryInnerQueryPage.vue
@@ -1,10 +1,28 @@
 <template>
   <div class="demo-split">
     <Split v-model="leftRightSplit" mode="horizontal" min="325px">
-      <div slot="left" class="demo-split-pane">Left Pane</div>
+      <div slot="left" class="demo-split-pane">
+        <div>
+          <Input
+            type="textarea"
+            :autosize="{minRows: 30,maxRows: 30}"
+            placeholder="请输入单号，用逗号分隔，最多50个"
+          ></Input>
+        </div>
+        <div>
+          <div style="width:100%;">
+            <h3 style="text-align:left">还可以输入30个单号</h3>
+          </div>
+        </div>
+        <br />
+        <div style="width:100%">
+          <Button type="primary" size="large" style="width:130px; margin-left: 10px">清空</Button>
+          <Button type="primary" size="large" style="width:130px; margin-left: 10px">查询</Button>
+        </div>
+      </div>
       <div slot="right" class="demo-split-pane">
-        <!-- <div>分类TODO</div> -->
-        <!-- <Divider /> -->
+        <div>分类TODO</div>
+        <Divider />
         <div>查询列表</div>
       </div>
     </Split>
